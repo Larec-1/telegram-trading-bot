2e1111ef120d178ab9e346b7b522de4a24bb62c9
Larec-1 <sergienkojaroslav@gmail.com>
2025-04-21 22:07:49 +0300

    Add files via upload

---
+++ gbot.py
@@ -0,0 +1,25 @@
+from flask import Flask, request
+import requests
+
+app = Flask(__name__)
+
+TOKEN = '8065007276:AAGM5_qHN7W_DVdmewlHUPZPXx_7p0hTTE8'
+TELEGRAM_API_URL = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+
+@app.route('/')
+def home():
+    return 'Бот работает!'
+
+@app.route(f'/{TOKEN}', methods=['POST'])
+def webhook():
+    data = request.get_json()
+    if 'message' in data:
+        chat_id = data['message']['chat']['id']
+        text = data['message'].get('text', '')
+        if text:
+            reply = f"Ты написал: {text}"
+            requests.post(TELEGRAM_API_URL, json={'chat_id': chat_id, 'text': reply})
+    return {'ok': True}
+
+if __name__ == '__main__':
+    app.run()
